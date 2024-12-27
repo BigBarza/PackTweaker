9673d73ccbc67e3f09842112bc6deccb0d008f9f
Killerqu00 <killerqueen1777@gmail.com>
2024-12-27 18:10:53 +0100

    architectury example completed

---
+++ common/src/main/java/com/killerqu/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.killerqu;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "packtweaker";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
