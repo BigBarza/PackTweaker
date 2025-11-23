426d76358ab5420f25477a94363f44d59c91598f
murphyneko <killerqueen1777@gmail.com>
2025-11-23 22:31:11 +0100

    1.20.1 update

--- src/main/java/com/killerqu/packtweaker/mixin/ForgeIngameGuiMixin.java
+++ src/main/java/com/killerqu/packtweaker/mixin/ForgeIngameGuiMixin.java
@@ -2,6 +2,7 @@ package com.killerqu.packtweaker.mixin;
 
 import com.killerqu.packtweaker.config.CommonConfig;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = ForgeGui.class, remap = false)
 public class ForgeIngameGuiMixin {
     @Inject(method = "renderFood", at = @At("HEAD"), cancellable = true)
-    public void packtweaker$renderHunger(int width, int height, PoseStack poseStack, CallbackInfo ci) {
+    public void packtweaker$renderHunger(int width, int height, GuiGraphics guiGraphics, CallbackInfo ci) {
         if (!CommonConfig.ENABLE_HUNGER.get()) {
             ci.cancel();
         }
